1439122df11cdead6b3175e7f6b82878c0da52d6
Hridoykhan4 <hridoykhan148385@gmail.com>
2024-11-03 18:24:11 +0600

    Added add Money portion

--- scripts/home.js
+++ scripts/home.js
@@ -1,1 +1,17 @@
-console.log('Added')
+/* 
+Add event handler
+prevent page load
+get money to be added to the balance
+*/
+
+
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+    event.preventDefault()
+
+    const addMoneyInput = document.getElementById('input-add-money').value;
+    console.log(addMoneyInput);
+
+    // Get The Pin Number
+    const pinNumberInput = document.getElementById('input-pin-number').value;
+    console.log(pinNumberInput)
+});
