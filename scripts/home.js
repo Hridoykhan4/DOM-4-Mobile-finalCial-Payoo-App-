9a3299ef91a4c66be227d1a47eb75cbea704bf60
Hridoykhan4 <hridoykhan148385@gmail.com>
2024-11-03 18:47:26 +0600

    Practing Done

--- scripts/home.js
+++ scripts/home.js
@@ -2,6 +2,8 @@
 Add event handler
 prevent page load
 get money to be added to the balance
+get the pin
+Verify pin
 */
 
 
@@ -9,9 +11,28 @@ document.getElementById('btn-add-money').addEventListener('click', function(even
     event.preventDefault()
 
     const addMoneyInput = document.getElementById('input-add-money').value;
-    console.log(addMoneyInput);
+    
 
     // Get The Pin Number
     const pinNumberInput = document.getElementById('input-pin-number').value;
-    console.log(pinNumberInput)
+ 
+    // Verify Pin Number
+    if(pinNumberInput === '1234'){
+
+        // Step 4 Get the Current Balance
+        const balance = document.getElementById('account-balance').innerText;
+
+        
+        // Step 5 Add addmoneyInput with main balance
+        const addMoneyNumber = parseFloat(addMoneyInput);
+        const balanceNumber = parseFloat(balance)
+        const newBalance = addMoneyNumber + balanceNumber
+       
+        // Step 6 Update the balance in UI
+        document.getElementById('account-balance').innerText = newBalance
+    }
+    else{
+        alert('Please Try Again')
+    }
+
 });
